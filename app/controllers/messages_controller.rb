315340a6d2485539ce1b5abba20db6e7f8c28934
shingoi <shingo_iwama@yahoo.co.jp>
2016-07-16 02:00:22 +0000

    Implementation of destroy action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     @message = Message.new
     # Messageを全て取得する。
@@ -16,10 +18,29 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path, notice: 'メッセージを編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
 
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  ## ここまで
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
